185a46c30bf3f6020d3ce40642247cf57138c265
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro.local>
2017-05-31 18:37:01 -0700

    Resubmit Lab2

--- app.js
+++ app.js
@@ -1,48 +1,58 @@
 'use strict';
 
-var question1 = prompt('Do I have siblings? Answer YES or NO');
+var question1 = prompt('Do I have siblings? Answer yes or no, y or n');
 
-if (question1.toUpperCase() === 'YES') {
-  console.log('Correct! High five buddy!');
+if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
+  alert('Correct! High five buddy!');
 } else {
-  console.log('I thought you were my friend :(');
+  alert('I thought you were my friend :(');
 };
-var question2 = prompt('Is crimson my favorite color? Answer YES or NO');
+var question2 = prompt('Is crimson my favorite color? Answer yes or no, y or n');
 
-if (question2.toUpperCase() === 'YES') {
-  console.log('Correct! I bleed Crimson. GO COUGS!');
+if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
+  alert('Correct! I bleed Crimson. GO COUGS!');
 } else {
-  console.log('I thought you were my friend :(');
-};
-
-var question3 = prompt('Did I eat Oatmel for breafast on Tuesday? Answer YES or NO');
-
-if (question3.toUpperCase() === 'NO') {
-  console.log('Correct! I ate donuts!');
-} else {
-  console.log('I thought you were my friend :(');
+  alert('I thought you were my friend :(');
 };
 
-var question4 = prompt('Is Pikachu my highest level Pokemon in Pokemon Crystal? Answer YES or NO');
+var question3 = prompt('Did I eat Oatmel for breafast on Tuesday? Answer yes or no, y or n');
 
-if (question4.toUpperCase() === 'NO') {
-  console.log('Correct! Nidorino is my highest level Pokemon! Until I find a moon stone...');
+if (question3.toUpperCase() === 'NO' || question3.toUpperCase() === 'N') {
+alert('Correct! I ate donuts!');
 } else {
-  console.log('I thought you were my friend :(');
+alert('I thought you were my friend :(');
 };
 
-var question5 = prompt('Is Ashley Graham my celebrity crush? Answer YES or NO');
+var question4 = prompt('Is Pikachu my highest level Pokemon in Pokemon Crystal? Answer yes or no, y or n');
 
-if (question5.toUpperCase() === 'YES') {
-  console.log('Correct! She is very beautiful!');
+if (question4.toUpperCase() === 'NO' || question4.toUpperCase() === 'N') {
+alert('Correct! Nidorino is my highest level Pokemon! Until I find a moon stone...');
 } else {
-  console.log('I thought you were my friend :(');
+alert('I thought you were my friend :(');
 };
+
+var question5 = prompt('Is Ashley Graham my celebrity crush? Answer yes or no, y or n');
+
+if (question5.toUpperCase() === 'YES' || question5.toUpperCase() === 'Y') {
+alert('Correct! She is very beautiful!');
+} else {
+alert('I thought you were my friend :(');
+};
+
+
+document.write('<h2> Do I have siblings? Yes!</h2>');
+document.write('<h2>Is crimson my favorite color? Yes!</h2>');
+document.write('<h2>Did I eat Oatmel for breafast on Tuesday? No!</h2>');
+document.write('<h2>Is Pikachu my highest level Pokemon in Pokemon Crystal? No!</h2>');
+document.write('<h2>Is Ashley Graham my celebrity crush? Yes!</h2>');
+document.write('<h1> </h1>');
+document.write('<h1>How well did you do: </h1>');
+
 
 var outcome = question1.length + question2.length + question3.length + question4.length + question5.length
 
 if (outcome > 12) {
-  document.write('You are friend!');
+document.write('You are my friend!');
 } else {
 document.write('I hate you.');
 };
