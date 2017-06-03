96de122b3e958a35376b0f31cee60a8667bba933
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro.local>
2017-06-03 13:24:04 -0700

    Fixed Linter Errors

--- app.js
+++ app.js
@@ -18,27 +18,26 @@ if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
 var question3 = prompt('Did I eat Oatmel for breafast on Tuesday? Answer yes or no, y or n');
 
 if (question3.toUpperCase() === 'NO' || question3.toUpperCase() === 'N') {
-alert('Correct! I ate donuts!');
+  alert('Correct! I ate donuts!');
 } else {
-alert('I thought you were my friend :(');
+  alert('I thought you were my friend :(');
 };
 
 var question4 = prompt('Is Pikachu my highest level Pokemon in Pokemon Crystal? Answer yes or no, y or n');
 
 if (question4.toUpperCase() === 'NO' || question4.toUpperCase() === 'N') {
-alert('Correct! Nidorino is my highest level Pokemon! Until I find a moon stone...');
+  alert('Correct! Nidorino is my highest level Pokemon! Until I find a moon stone...');
 } else {
-alert('I thought you were my friend :(');
+  alert('I thought you were my friend :(');
 };
 
 var question5 = prompt('Is Ashley Graham my celebrity crush? Answer yes or no, y or n');
 
 if (question5.toUpperCase() === 'YES' || question5.toUpperCase() === 'Y') {
-alert('Correct! She is very beautiful!');
+  alert('Correct! She is very beautiful!');
 } else {
-alert('I thought you were my friend :(');
+  alert('I thought you were my friend :(');
 };
-
 
 document.write('<h2> Do I have siblings? Yes!</h2>');
 document.write('<h2>Is crimson my favorite color? Yes!</h2>');
@@ -48,11 +47,10 @@ document.write('<h2>Is Ashley Graham my celebrity crush? Yes!</h2>');
 document.write('<h1> </h1>');
 document.write('<h1>How well did you do: </h1>');
 
-
-var outcome = question1.length + question2.length + question3.length + question4.length + question5.length
+var outcome = question1.length + question2.length + question3.length + question4.length + question5.length;
 
 if (outcome > 12) {
-document.write('You are my friend!');
+  document.write('You are my friend!');
 } else {
-document.write('I hate you.');
+  document.write('I hate you.');
 };
